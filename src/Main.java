d76f7b394a4bf18fe03ca86d673cc947eeb41db7
IrynaMe <irinam7a@gmail.com>
2024-04-24 11:52:51 +0200

    2 commit

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,14 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Licantropo l = new Licantropo(true);
+        Eroe e = new Eroe();
+        System.out.println(e.getForza());
+        System.out.println(l.getForza());
+        l.azzanna();
+        e.combatti();
+        System.out.println(e.getForza());
+        System.out.println(l.getForza());
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
     }
 }
